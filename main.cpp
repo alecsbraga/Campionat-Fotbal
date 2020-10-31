67695c562ce79fb6dc1268d4518263281116e923
alecsbraga <62885146+alecsbraga@users.noreply.github.com>
2020-10-31 16:52:01 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,12 +6,16 @@ using namespace std;
 class Echipa
 {
     string nume_echipa;
-    int nr_partide, nr_partide_castigate, nr_partide_egale, nr_partide_pierdute, punctaj;
+    int nr_partide_castigate, nr_partide_egale, nr_partide_pierdute;
 
 public:
-    void a_jucat_un_meci()
+    int* nr_partide()
     {
-        nr_partide++;
+        int *nr_partidetotal;
+        nr_partidetotal=new int;
+        *nr_partidetotal=nr_partide_castigate+nr_partide_egale+nr_partide_pierdute;
+        return nr_partidetotal;
+        delete nr_partidetotal;
     }
 
     void a_castigat_un_meci()
@@ -27,14 +31,18 @@ public:
     {
         nr_partide_egale++;
     }
-    void punctaj_calculat()
+    int* punctaj_calculat()
     {
-        punctaj=nr_partide_castigate*3+nr_partide_egale;
+        int *puntajtotal;
+        puntajtotal=new int;
+        *puntajtotal=nr_partide_castigate*3+nr_partide_egale;
+        return puntajtotal;
+        delete puntajtotal;
 
     }
     void afisare_date()
     {
-        cout<<nume_echipa<<" "<<nr_partide<<" "<<nr_partide_castigate<<" "<<nr_partide_egale<<" "<<nr_partide_pierdute<<" "<<punctaj<<'\n';
+        cout<<nume_echipa<<" "<<*nr_partide()<<" "<<nr_partide_castigate<<" "<<nr_partide_egale<<" "<<nr_partide_pierdute<<" "<<*punctaj_calculat()<<'\n';
     }
     string get_nume()
     {
@@ -52,7 +60,7 @@ public:
 
     Echipa(string, int, int, int);
      */
-    Echipa(string nume_echipa = "noname", int nr_partide = 0, int nr_partide_castigate = 0, int nr_partide_egale = 0, int nr_partide_pierdute = 0);
+    Echipa(string nume_echipa = "noname", int nr_partide_castigate = 0, int nr_partide_egale = 0, int nr_partide_pierdute=0);
 
     Echipa(Echipa &);
 
@@ -115,25 +123,22 @@ Echipa::Echipa(string nume_echipa, int nr_partide, int nr_partide_castigate, int
     afisare_date();
 }
 */
-Echipa::Echipa(string nume_echipa, int nr_partide, int nr_partide_castigate, int nr_partide_egale, int nr_partide_pierdute)
+Echipa::Echipa(string nume_echipa, int nr_partide_castigate, int nr_partide_egale,int nr_partide_pierdute)
 {
     this->nume_echipa = nume_echipa;
-    this->nr_partide = nr_partide;
     this->nr_partide_castigate = nr_partide_castigate;
     this->nr_partide_egale = nr_partide_egale;
     this->nr_partide_pierdute = nr_partide_pierdute;
-    punctaj_calculat();
+
 
 }
 
 Echipa::Echipa(Echipa &echipa)
 {
     this->nume_echipa = echipa.nume_echipa;
-    this->nr_partide = echipa.nr_partide;
     this->nr_partide_castigate = echipa.nr_partide_castigate;
     this->nr_partide_egale = echipa.nr_partide_egale;
     this->nr_partide_pierdute = echipa.nr_partide_pierdute;
-    this->punctaj = echipa.punctaj;
 
 }
 
@@ -209,10 +214,10 @@ class Meci{
         if(castiga == 1)
         {
             cout<<"In urma meciului dintre " <<echipa_1.get_nume()<<" si "<< echipa_2.get_nume()<<", a castigta "<< echipa_1.get_nume();
-            echipa_1.a_jucat_un_meci();
             echipa_1.a_castigat_un_meci();
-            echipa_2.a_jucat_un_meci();
+            echipa_1.nr_partide();
             echipa_2.a_pierdut_un_meci();
+            echipa_2.nr_partide();
             echipa_1.punctaj_calculat();
             cout<<endl<<"Datele sunt urmatoarele:"<<endl;
             echipa_1.afisare_date();
@@ -222,10 +227,10 @@ class Meci{
         else if(castiga == 2)
         {
             cout<<"In urma meciului dintre " <<echipa_1.get_nume()<<" si "<< echipa_2.get_nume()<<", s-a terminat egal";
-            echipa_1.a_jucat_un_meci();
             echipa_1.a_facut_egal();
-            echipa_2.a_jucat_un_meci();
             echipa_2.a_facut_egal();
+            echipa_1.nr_partide();
+            echipa_1.nr_partide();
             echipa_1.punctaj_calculat();
             echipa_2.punctaj_calculat();
             cout<<endl<<"Datele sunt urmatoarele:"<<endl;
@@ -235,10 +240,10 @@ class Meci{
         else
         {
             cout<<"In urma meciului dintre " <<echipa_1.get_nume()<<" si "<< echipa_2.get_nume()<<", a castigat "<< echipa_2.get_nume();
-            echipa_2.a_jucat_un_meci();
             echipa_2.a_castigat_un_meci();
-            echipa_1.a_jucat_un_meci();
             echipa_1.a_pierdut_un_meci();
+            echipa_1.nr_partide();
+            echipa_1.nr_partide();
             echipa_2.punctaj_calculat();
             cout<<endl<<"Datele sunt urmatoarele:"<<endl;
             echipa_1.afisare_date();
@@ -250,8 +255,8 @@ class Meci{
 
  main()
 {
-    Echipa echipa_1("FCSB", 5, 4, 1);
-    Echipa echipa_2("FC Dinamo Bucuresti", 5, 2, 2, 1);
+    Echipa echipa_1("FCSB",4, 1);
+    Echipa echipa_2("FC Dinamo Bucuresti",2, 2, 1);
     echipa_1.afisare_date();
     echipa_2.afisare_date();
     Patron patron_1("Gigi Becali", echipa_1);
