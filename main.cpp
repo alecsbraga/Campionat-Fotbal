6dad815e831d4aad33bc2d2bf1c34f711b61623e
alecsbraga <62885146+alecsbraga@users.noreply.github.com>
2020-11-01 12:48:04 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,13 +9,9 @@ class Echipa
     int nr_partide_castigate, nr_partide_egale, nr_partide_pierdute;
 
 public:
-    int* nr_partide()
+    int nr_partide()
     {
-        int *nr_partidetotal;
-        nr_partidetotal=new int;
-        *nr_partidetotal=nr_partide_castigate+nr_partide_egale+nr_partide_pierdute;
-        return nr_partidetotal;
-        delete nr_partidetotal;
+       return nr_partide_castigate+nr_partide_egale+nr_partide_pierdute;
     }
 
     void a_castigat_un_meci()
@@ -31,20 +27,14 @@ public:
     {
         nr_partide_egale++;
     }
-    int* punctaj_calculat()
+    int punctaj_calculat()
     {
-        int *puntajtotal;
-        puntajtotal=new int;
-        *puntajtotal=nr_partide_castigate*3+nr_partide_egale;
-        return puntajtotal;
-        delete puntajtotal;
+        return nr_partide_castigate*3+nr_partide_egale;
 
     }
     void afisare_date()
     {
-        cout<<nume_echipa<<" "<<*nr_partide()<<" "<<nr_partide_castigate<<" "<<nr_partide_egale<<" "<<nr_partide_pierdute<<" "<<*punctaj_calculat()<<'\n';
-        delete nr_partide();
-        delete punctaj_calculat();
+        cout<<nume_echipa<<" "<<nr_partide()<<" "<<nr_partide_castigate<<" "<<nr_partide_egale<<" "<<nr_partide_pierdute<<" "<<punctaj_calculat()<<'\n';
     }
     string get_nume()
     {
@@ -141,16 +131,12 @@ Echipa::Echipa(Echipa &echipa)
     this->nr_partide_castigate = echipa.nr_partide_castigate;
     this->nr_partide_egale = echipa.nr_partide_egale;
     this->nr_partide_pierdute = echipa.nr_partide_pierdute;
-    delete nr_partide();
-    delete punctaj_calculat();
 
 }
 
 Echipa::~Echipa()
 {
     //  cout<<'\n'<<"Gata cu echipa "<<nume_echipa<<'\n';
-    delete nr_partide();
-    delete punctaj_calculat();
 }
 
 class Patron
@@ -162,8 +148,6 @@ public:
     {
         cout<<"Patron: "<<nume_patron<<" "<<", club: ";
         echipa.afisare_date();
-        delete echipa.nr_partide();
-        delete echipa.punctaj_calculat();
     }
     Patron(string nume_patron, Echipa echipa);
     ~Patron();
@@ -175,15 +159,13 @@ Patron::Patron(string nume_patron, Echipa echipa)
     this->nume_patron = nume_patron;
     this->echipa = echipa;
     afisare_date();
-    delete echipa.nr_partide();
-    delete echipa.punctaj_calculat();
+
 }
 
 Patron::~Patron()
 {
     // cout<<"Gata cu patronul "<<nume_patron<<endl;
-    delete echipa.nr_partide();
-    delete echipa.punctaj_calculat();
+
 }
 
 class Stadion
@@ -196,8 +178,6 @@ public:
     {
         cout<<"Stadion: "<<nume_stadion<<" Gazda: ";
         echipa.afisare_date();
-        delete echipa.nr_partide();
-        delete echipa.punctaj_calculat();
 
     }
 
@@ -210,16 +190,12 @@ Stadion::Stadion(string nume_stadion,Echipa echipa)
 {
     this->nume_stadion = nume_stadion;
     this->echipa=echipa;
-    delete echipa.nr_partide();
-    delete echipa.punctaj_calculat();
 
 }
 
 Stadion::~Stadion()
 {
     // cout<<"Gata cu stadionul " << nume_stadion;
-    delete echipa.nr_partide();
-    delete echipa.punctaj_calculat();
 }
 
 class Meci
@@ -227,58 +203,54 @@ class Meci
     Echipa echipa_1;
     Echipa echipa_2;
 public:
+    void cine_a_castigat(Echipa castigatorea, Echipa invinsa, bool remiza)
+    {
+        if(remiza == false)
+        cout<<"In urma meciului dintre " <<castigatorea.get_nume()<<" si "<< invinsa.get_nume()<<", a castigta "<< castigatorea.get_nume();
+        else
+            cout<<cout<<"In urma meciului dintre " <<castigatorea.get_nume()<<" si "<< invinsa.get_nume()<<", s-a terminat egal";
+    }
     void meci(Echipa &echipa_1, Echipa &echipa_2)
     {
         int castiga= 1 + (rand() % ( 3 - 1 + 1 ) );
+        bool remiza;
         if(castiga == 1)
         {
-            cout<<"In urma meciului dintre " <<echipa_1.get_nume()<<" si "<< echipa_2.get_nume()<<", a castigta "<< echipa_1.get_nume();
+            remiza= false;
+            cine_a_castigat(echipa_1, echipa_2, remiza);
             echipa_1.a_castigat_un_meci();
-            echipa_1.nr_partide();
             echipa_2.a_pierdut_un_meci();
             echipa_2.nr_partide();
             echipa_1.punctaj_calculat();
             cout<<endl<<"Datele sunt urmatoarele:"<<endl;
             echipa_1.afisare_date();
             echipa_2.afisare_date();
-            delete echipa_1.nr_partide();
-            delete echipa_1.punctaj_calculat();
-            delete echipa_2.nr_partide();
-            delete echipa_2.punctaj_calculat();
 
         }
         else if(castiga == 2)
         {
-            cout<<"In urma meciului dintre " <<echipa_1.get_nume()<<" si "<< echipa_2.get_nume()<<", s-a terminat egal";
+            remiza= true;
+            cine_a_castigat(echipa_1, echipa_2, remiza);
             echipa_1.a_facut_egal();
             echipa_2.a_facut_egal();
-            echipa_1.nr_partide();
-            echipa_1.nr_partide();
             echipa_1.punctaj_calculat();
             echipa_2.punctaj_calculat();
             cout<<endl<<"Datele sunt urmatoarele:"<<endl;
             echipa_1.afisare_date();
             echipa_2.afisare_date();
-            delete echipa_1.nr_partide();
-            delete echipa_1.punctaj_calculat();
-            delete echipa_2.nr_partide();
-            delete echipa_2.punctaj_calculat();
         }
         else
         {
-            cout<<"In urma meciului dintre " <<echipa_1.get_nume()<<" si "<< echipa_2.get_nume()<<", a castigat "<< echipa_2.get_nume();
+            remiza= false;
+            cine_a_castigat(echipa_2, echipa_1, remiza);
             echipa_2.a_castigat_un_meci();
             echipa_1.a_pierdut_un_meci();
-            echipa_1.nr_partide();
-            echipa_1.nr_partide();
             echipa_2.punctaj_calculat();
             cout<<endl<<"Datele sunt urmatoarele:"<<endl;
             echipa_1.afisare_date();
             echipa_2.afisare_date();
-            delete echipa_1.nr_partide();
-            delete echipa_1.punctaj_calculat();
-            delete echipa_2.nr_partide();
-            delete echipa_2.punctaj_calculat();
+
+
         }
 
     }
@@ -287,10 +259,7 @@ public:
 
 Meci::~Meci()
 {
-    delete echipa_1.nr_partide();
-    delete echipa_1.punctaj_calculat();
-    delete echipa_2.nr_partide();
-    delete echipa_2.punctaj_calculat();
+
 }
 
 main()
@@ -298,11 +267,7 @@ main()
     Echipa echipa_1("FCSB",4, 1);
     Echipa echipa_2("FC Dinamo Bucuresti",2, 2, 1);
     echipa_1.afisare_date();
-    delete echipa_1.nr_partide();
-    delete echipa_1.punctaj_calculat();
     echipa_2.afisare_date();
-    delete echipa_2.nr_partide();
-    delete echipa_2.punctaj_calculat();
     Patron patron_1("Gigi Becali", echipa_1);
     Patron patron_2("Pablo Cortacero", echipa_2);
     Stadion stadion_1("Ghencea",echipa_1);
