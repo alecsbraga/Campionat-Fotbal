04428fecc06ea578a8fa8f061b7456c16b58c4f7
alecsbraga <62885146+alecsbraga@users.noreply.github.com>
2020-11-02 23:30:20 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -266,8 +266,12 @@ int main()
 {
     Echipa echipa_1("FCSB", 4, 1);
     Echipa echipa_2("FC Dinamo Bucuresti",2, 2, 1);
+    Echipa echipa_3 = echipa_1;
     echipa_1.afisare_date();
     echipa_2.afisare_date();
+    echipa_3.afisare_date();
+    Echipa echipa_4(echipa_2);
+    echipa_4.afisare_date();
     Patron patron_1("Gigi Becali", echipa_1);
     Patron patron_2("Pablo Cortacero", echipa_2);
     Stadion stadion_1("Ghencea",echipa_1);
