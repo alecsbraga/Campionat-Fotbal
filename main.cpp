c56ca1740ec1c7af5c40e8fd1ca6bfe17bc87708
alecsbraga <62885146+alecsbraga@users.noreply.github.com>
2020-11-23 19:56:55 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,38 +7,47 @@
 #include <fstream>
 #include "Campionat.h"
 #include <typeinfo>
+#include <memory>
 
 using namespace std;
 
 int main()
 {
-    Echipa echipa_1("FCSB", 4, 1);
+    auto_ptr<Echipa>echipa_1(new Echipa("FCSB", 4, 1));
     Echipa echipa_2("FC Dinamo Bucuresti",2, 2, 1);
     Echipa echipa_3("CFR Cluj",3,1,1);
-    cout<<echipa_1;
+    cout<<*echipa_1;
     cout<<echipa_2;
     cout<<echipa_3;
-    Patron patron_1("Gigi Becali", echipa_1);
+    Patron patron_1("Gigi Becali", *echipa_1);
     Patron patron_2("Pablo Cortacero", echipa_2);
-    Stadion stadion_1("Ghencea",echipa_1);
+    Stadion stadion_1("Ghencea", *echipa_1);
     Stadion stadion_2("Stefan Cel Mare",echipa_2);
-    Campionat campionat;
+    auto_ptr<Campionat> campionat (new Campionat());
     ifstream f;
     try
     {
         f.open("Echipe.in");
-        f>>campionat;
+
+        f>>*campionat;
         try
         {
-            campionat.joaca();
+            campionat->joaca();
         }
         catch(const runtime_error& e)
         {
             cout<<e.what()<<endl;
         }
-        campionat.ordoneaza();
-        cout<<campionat;
-        campionat.cine_a_castigat();
+        try
+        {
+            campionat->ordoneaza();
+        }
+        catch(const runtime_error& e)
+        {
+            cout<<e.what()<<endl;
+        }
+        cout<<*campionat;
+        campionat->cine_a_castigat();
 
     }
     catch (const ifstream::failure& e)
